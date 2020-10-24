ffa287f0adbe2cd7aa99377d235c1e04da6152cc
Roman <roman.travin@magenta-technology.com>
2020-10-25 00:53:58 +0400

    Добавлено задание 4.1

---
+++ 41-python3-Samoylenko.py
@@ -0,0 +1,8 @@
+
+# Задание 4.1
+# Обработать строку NAT таким образом, чтобы в имени интерфейса 
+# вместо FastEthernet было GigabitEthernet.
+
+NAT = "ip nat inside source list ACL interface FastEthernet0/1 overload"
+
+print(NAT)
