73448cce612e949e7fd17d6a2b52c94e9034dd78
Roman <roman.travin@magenta-technology.com>
2020-11-22 23:32:05 +0400

    Выполено задание 4.6

--- 46-python3-Samoylenko.py
+++ 46-python3-Samoylenko.py
@@ -11,3 +11,24 @@
 
 ospf_route = 'O     10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
 
+clear_route = ospf_route.replace("[","").replace("]","").replace(","," ").split()
+
+clear_route[0] = "OSPF"
+
+templ_format = """
+Protocol:           {0:20}
+Prefix:             {1:20}
+AD/Metric:          {2:20}
+Next-Hop:           {3:20}
+Last update:        {4:20}
+Outbound Interface: {5:20}
+"""
+
+print(templ_format.format(
+    clear_route[0],
+    clear_route[1],
+    clear_route[2],
+    clear_route[4],
+    clear_route[5],
+    clear_route[6]
+    ))
