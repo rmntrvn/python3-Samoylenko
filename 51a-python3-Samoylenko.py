e4dac5dc7685fa51d16d60bb51e11d23a3e1ccbc
Roman <roman.travin@magenta-technology.com>
2020-11-29 23:50:16 +0400

    Добавлено решение 5.1a

--- 51a-python3-Samoylenko.py
+++ 51a-python3-Samoylenko.py
@@ -23,3 +23,32 @@
 # 11111111 11111111 11111111 00000000
 #
 # Проверить работу скрипта на разных комбинациях сеть/маска.
+
+IP_MASK_INPUT = input('Введите IP адрес сети в формате: 10.1.1.0/24\n')
+
+IP_HOST = IP_MASK_INPUT.split('/')[0].split('.')
+MASK = IP_MASK_INPUT.split('/')[1]
+
+BINARY_MASK_LINE = str('1' * int(MASK))
+FULL_BINARY_MASK_LINE = "{:<032}".format(BINARY_MASK_LINE)
+
+IP_NETWORK = [ "{:>08}".format(str( int(IP_HOST[0]) & int(FULL_BINARY_MASK_LINE[0:8], 2))),
+               "{:>08}".format(str( int(IP_HOST[1]) & int(FULL_BINARY_MASK_LINE[8:16], 2))),
+               "{:>08}".format(str( int(IP_HOST[2]) & int(FULL_BINARY_MASK_LINE[16:24], 2))),
+               "{:>08}".format(str( int(IP_HOST[3]) & int(FULL_BINARY_MASK_LINE[16:32], 2)))]
+
+TEMPL_TABLE = """{0:<8} {1:<8} {2:<8} {3:<8}
+{0:0>8b} {1:0>8b} {2:0>8b} {3:0>8b}"""
+
+print("\nNetwork:")
+print(TEMPL_TABLE.format(int(IP_NETWORK[0], 10),
+                         int(IP_NETWORK[1], 10),
+                         int(IP_NETWORK[2], 10), 
+                         int(IP_NETWORK[3], 10)))
+print("\nMask:")
+print("/" + MASK)
+
+print(TEMPL_TABLE.format(int(FULL_BINARY_MASK_LINE[0:8], 2),
+                         int(FULL_BINARY_MASK_LINE[8:16], 2),
+                         int(FULL_BINARY_MASK_LINE[16:24], 2), 
+                         int(FULL_BINARY_MASK_LINE[24:32], 2)))
